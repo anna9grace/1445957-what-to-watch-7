57f5d37443824d7d9b278eb8ca7d34683b8d301b
anna9grace <anna9grace@gmail.com>
2021-06-11 13:29:29 +0300

    создает компоненты

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import MainScreen from '../main-screen/main-screen';
 import PropTypes from 'prop-types';
+import SignInScreen from '../sign-in-screen/sign-in-screen';
+import MyListScreen from '../my-list-screen/my-list-screen';
+import FilmScreen from '../film-screen/film-screen';
+import AddReviewScreen from '../add-review-screen/add-review-screen';
+import PlayerScreen from '../player-screen/player-screen';
 
 function App(props) {
   const {promoFilm} = props;
 
   return (
-    <MainScreen promoFilm={promoFilm} />
+    // <MainScreen promoFilm={promoFilm} />
+    // <SignInScreen/>
+    // <MyListScreen />
+    // <FilmScreen />
+    // <AddReviewScreen />
+    <PlayerScreen />
   );
 }
 
