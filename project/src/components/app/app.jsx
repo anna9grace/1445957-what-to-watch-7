ed89b2afeaa60b7f48d7c4e06ab553fbddefa4e1
anna9grace <anna9grace@gmail.com>
2021-06-11 14:16:49 +0300

    выполняет маршрутизацию приложения

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {AppRoute} from '../../const';
 import MainScreen from '../main-screen/main-screen';
 import PropTypes from 'prop-types';
 import SignInScreen from '../sign-in-screen/sign-in-screen';
@@ -6,17 +8,43 @@ import MyListScreen from '../my-list-screen/my-list-screen';
 import FilmScreen from '../film-screen/film-screen';
 import AddReviewScreen from '../add-review-screen/add-review-screen';
 import PlayerScreen from '../player-screen/player-screen';
+import NotFoundScreen from '../not-found-screen/not-found-screen';
 
 function App(props) {
   const {promoFilm} = props;
 
   return (
-    // <MainScreen promoFilm={promoFilm} />
-    // <SignInScreen/>
-    // <MyListScreen />
-    // <FilmScreen />
-    // <AddReviewScreen />
-    <PlayerScreen />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.ROOT}>
+          <MainScreen promoFilm={promoFilm}/>
+        </Route>
+
+        <Route exact path={AppRoute.SIGN_IN}>
+          <SignInScreen />
+        </Route>
+
+        <Route exact path={AppRoute.MY_LIST}>
+          <MyListScreen />
+        </Route>
+
+        <Route exact path={AppRoute.FILM}>
+          <FilmScreen />
+        </Route>
+
+        <Route exact path={AppRoute.ADD_REVIEW}>
+          <AddReviewScreen />
+        </Route>
+
+        <Route exact path={AppRoute.PLAYER}>
+          <PlayerScreen />
+        </Route>
+
+        <Route>
+          <NotFoundScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
